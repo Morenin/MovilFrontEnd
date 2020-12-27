75acfa9ee00eef825223b5f909b895fc48fa4aab
Morenin <49473045+Morenin@users.noreply.github.com>
2020-12-27 16:48:50 +0100

    inicio

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,8 +4,9 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   {
     path: '',
-    loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
-  }
+    loadChildren: () => import('./Alumno/tabs/tabs.module').then(m => m.TabsPageModule)
+  },
+  
 ];
 @NgModule({
   imports: [
