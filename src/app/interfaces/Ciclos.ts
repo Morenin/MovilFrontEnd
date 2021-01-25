392f95e4047c3571b75d7fef1c119c9a1d74dbb6
guilleballesteros <bugfiher@outlook.com>
2021-01-25 11:43:24 +0100

    cambios

--- src/app/interfaces/Ciclos.ts
+++ src/app/interfaces/Ciclos.ts
@@ -1,25 +1,6 @@
-import { Url } from "url";
 
 export interface Ciclos{
     id: number,
     name: String,
     img: String
-}
-
-export interface Ofertas{
-    id: number,
-    headline: string,
-    description: string,
-    date_max: Date,
-    num_candidates: number,
-}
-
-export interface Noticias{
-    id: number,
-    title: string,
-    description: string,
-    image: string,
-    cicle_id: number,
-    cicle_name: string,
-    created_at: Date,
 }
